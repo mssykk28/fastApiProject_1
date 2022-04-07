16f84b37f764e02318a4a9da336815dd3b57906e
masashi <mss.nt.2828@gmail.com>
2022-04-07 21:00:54 +0900

    http_triggerの修正

--- http_trigger/fast_api.py
+++ http_trigger/fast_api.py
@@ -1,44 +1,14 @@
-from typing import Optional
-
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
+from src.api.presentation.controller import example_controller as examples
 
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-
-
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.get("/hello/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.post("/hello")
-async def create_item(item: Item):
-    return item
-
-
-@app.put("/hello/{item_id}")
-async def update_item(item_id: int, item: Item):
-    return {"item_id": item_id}
-
-
-@app.delete("/hello/{item_id}")
-async def delete_item(item_id: int):
-    return {"item_id": item_id}
+fast_app = FastAPI()
+fast_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+fast_app.include_router(examples.router)
